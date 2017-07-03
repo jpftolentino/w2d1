908c7a2dfec9fa43fb172f3ce2f04cf242e6ac30
John Tolentino <jpftolentino@gmail.com>
2017-07-03 15:13:14 +0000

    scripting-http-request Step 3 done

---
+++ scripting-http-request/getAndPrintHTMLstep3.js
@@ -0,0 +1,35 @@
+/*
+
+  - Function accepts parameter option
+
+*/
+
+var https = require('https');
+
+
+function getAndPrintHTML (options) {
+
+var bufferedData = "";
+https.get(requestOptions, function (response) {
+
+  response.setEncoding('utf8');
+
+  response.on('data', function (data) {
+    bufferedData += data;
+  });
+
+  response.on('end', function() {
+    console.log(bufferedData);
+  });
+
+});
+
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step2.html'
+}
+
+
+getAndPrintHTML(requestOptions);
